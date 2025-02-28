d504c836597a8d2e839f11237c4f183d296bea14
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-28 15:30:22 +0530

    description placeholder updated

--- frontend/src/Components/AddTasks.jsx
+++ frontend/src/Components/AddTasks.jsx
@@ -23,7 +23,7 @@ function AddTasks() {
         <div onSubmit={handleSubmit} className='mt-5'>
             <form className='flex flex-col justify-center items-center gap-3'>
                 <input type="text" name='title' required value={formData?.title} onChange={handleChange} placeholder='write task title' className="input input-bordered w-[80%] md:w-[50%]" />
-                <input type="text" name='description' required value={formData?.description} onChange={handleChange} placeholder='Write you task here' className="input input-bordered w-[80%] md:w-[50%]" />
+                <input type="text" name='description' required value={formData?.description} onChange={handleChange} placeholder='Write your task here' className="input input-bordered w-[80%] md:w-[50%]" />
                 <button type='submit' className="bg-blue-500 w-[50%] md:w-[10%] px-3 py-2 md:py-3 rounded-md text-white">Add new task</button>
             </form>
         </div>
