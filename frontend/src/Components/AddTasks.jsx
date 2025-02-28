74ddc6c8b15ae37106214a5576a604f3d47ee869
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-28 19:13:29 +0530

    add task updated

--- frontend/src/Components/AddTasks.jsx
+++ frontend/src/Components/AddTasks.jsx
@@ -20,8 +20,8 @@ function AddTasks() {
     }
 
     return (
-        <div onSubmit={handleSubmit} className='mt-5'>
-            <form className='flex flex-col justify-center items-center gap-3'>
+        <div  className='mt-5'>
+            <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-3'>
                 <input type="text" name='title' required value={formData?.title} onChange={handleChange} placeholder='write task title' className="input input-bordered w-[80%] md:w-[50%]" />
                 <input type="text" name='description' required value={formData?.description} onChange={handleChange} placeholder='Write your task here' className="input input-bordered w-[80%] md:w-[50%]" />
                 <button type='submit' className="bg-blue-500 w-[50%] md:w-[10%] px-3 py-2 md:py-3 rounded-md text-white">Add new task</button>
